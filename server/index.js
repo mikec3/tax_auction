09c87482e45e7aa91a371e87de2fb2fe4738321e
Michael Couch <michaelcouch@MacBook-Pro.local>
2022-09-05 17:19:46 -0700

    boilerplate and google maps api key added to .env

---
+++ server/index.js
@@ -0,0 +1,40 @@
+// server/index.js
+
+const express = require("express");
+const path = require('path');
+const bodyParser = require("body-parser")
+require('dotenv').config();
+const http = require('http');
+const {Server} = require('socket.io');
+const axios = require('axios');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+// using this for reading webhook posts
+app.use(bodyParser.json())
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+// WEBSOCKETS!!!!
+// on connection console log the unique id for the client
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  console.log(socket.id);
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
