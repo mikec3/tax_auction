fe840754358319e0e5fecac2c0f2277b5c438db0
Michael Couch <michaelcouch@MacBook-Pro.local>
2022-10-12 14:52:58 -0700

    added readme

---
+++ server/Snohomish_Scraper.js
@@ -0,0 +1,13 @@
+// const express = require("express");
+// const path = require('path');
+// const bodyParser = require("body-parser")
+// require('dotenv').config();
+// const http = require('http');
+// const {Server} = require('socket.io');
+// const axios = require('axios');
+
+// const PORT = process.env.PORT || 3001;
+
+// const app = express();
+
+const selenium = require('selenium');
