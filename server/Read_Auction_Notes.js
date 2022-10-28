313aaea3fb55804963bd905f3b75af6f306447eb
Michael Couch <michaelcouch@MacBook-Pro.local>
2022-10-27 20:08:54 -0700

    new excel reader being tested

--- server/Read_Auction_Notes.js
+++ server/Read_Auction_Notes.js
@@ -1,32 +1,14 @@
 // This file will read the auction notes to give auction info to the scrapers
-
-const readXlsxFile = require('read-excel-file/node');
+const xlsx = require("xlsx");
 
 // Auction Notes file patch
 let filePath = 'Auction_Notes_2022.xlsx'
 
-const map = {
-  'COUNTY': 'COUNTY',
-  'AUCTION_DATE': 'AUCTION_DATE',
-  'AUCTION_INFO_URL': 'AUCTION_INFO_URL',
-  'PARCEL_INFO_URL': 'PARCEL_INFO_URL',
-  'AUCTION_SITE': 'AUCTION_SITE'
-}
+const workbook = xlsx.readFile(filePath);
+const sheetNames = workbook.SheetNames;
 
+// Get the data of "Sheet1"
+const data = xlsx.utils.sheet_to_json(workbook.Sheets['AuctionNotes'])
 
-const ReadData = async function () {
-
-let data = [];
-// Read excel file
-readXlsxFile(filePath, {map, sheet:'AuctionNotes'}).then((rows) => {
-  // `rows` is an array of rows
-  // each row being an array of cells.
-  data = rows;
-});
-
-return await data;
-}
-
-let data = ReadData();
 console.log(data);
 
