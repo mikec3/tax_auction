0dece1f621c9cff723d6f53db01148d4d18fa3dd
Michael Couch <michaelcouch@MacBook-Pro.local>
2022-12-30 18:49:27 -0800

    King County scraped basic, land, and tax

--- server/Coordinator.js
+++ server/Coordinator.js
@@ -1,4 +1,5 @@
 const Snohomish = require('./Snohomish_Scraper');
+const King = require('./King_Scraper');
 const Read_Auction_Notes = require('./Read_Auction_Notes');
 const Firebase = require('./UploadToFirebase.js');
 
@@ -6,6 +7,8 @@ const Firebase = require('./UploadToFirebase.js');
 // pass parcel list through snomohish_scraper and attach meta data to each parcel
 // Upload snohomish parcel info to firebase
 // get the auction meta data and snohomish parcel info
+
+// TODO getSnohomish() and getKing() are basically the same, refactor so that I'm not repeating code.
 
 let database = 'parcels_v2';
 
@@ -47,6 +50,49 @@ const getSnohomish = async function () {
 	return parcelOutput;
 }
 
+const getKing = async function () {
+	// Get Snohomish Parcel List
+	let KingParcelList = Read_Auction_Notes.readAuctionNotes('ParcelList').filter(item => item.COUNTY == 'King');
+
+	//  get auction meta data
+	let kingMetaData = Read_Auction_Notes.readAuctionNotes('AuctionNotes').filter(item => item.COUNTY == 'King')[0];
+
+	let parcelOutput = [];
+
+	// loop through snohomish parcel list
+	for await (const parcel of KingParcelList) {
+		console.log(parcel);
+
+		// add meta data to parcelObj
+		parcel['AUCTION_DATE'] = kingMetaData['AUCTION_DATE'];
+		parcel['AUCTION_INFO_URL'] = kingMetaData['AUCTION_INFO_URL'];
+		parcel['PARCEL_INFO_URL'] = kingMetaData['PARCEL_INFO_URL'];
+		parcel['PARCEL_DATA_BASE_URL'] = kingMetaData['PARCEL_DATA_BASE_URL'];
+
+		//for King County the parcel viewer can actually be constructed as concat(URL, parcel number)
+		parcel['PARCEL_VIEWER_URL'] = kingMetaData['PARCEL_VIEWER_URL']+parcel['PARCEL_NUM'];
+		parcel['AUCTION_SITE'] = kingMetaData['AUCTION_SITE'];
+		parcel['AUCTION_SITE_FLAG'] = kingMetaData['AUCTION_SITE_FLAG'];
+
+		// get the scraper data
+		let parcelInfo = await King.getParcelInfo(parcel['PARCEL_DATA_BASE_URL'],
+														parcel['PARCEL_VIEWER_URL'],
+														parcel['PARCEL_NUM']);
+		// add the metadata to the scraper data
+		parcelInfo['Meta'] = parcel;
+
+		// push the full parcel info onto the outputArray
+		parcelOutput.push(parcelInfo);
+
+		//TODO get rid of this break - for dev purposes to only go through first parcel in list.
+		break;
+
+	};
+
+	// return the output Array
+	return parcelOutput;
+}
+
 const Scrape = async function () {
 
 	// Uncomment out to re-scrape snohomish. last scrape was 10/12/22.
@@ -54,6 +100,10 @@ const Scrape = async function () {
 	// snohomish.forEach(parcel => {
 	// 	Firebase.upload(database, parcel);
 	// });
+
+	// get King County. Uncomment out to rescrape. Developing now.
+	let king = await getKing();
+	console.log(king);
 }
 
 Scrape();
---
+++ server/King_Scraper.js
@@ -0,0 +1,185 @@
+// Scrape King County
+// manually scrape parcel list and add to auction notes
+
+const webdriver = require('selenium-webdriver');
+const {By, until, Key} = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+
+
+// get info
+const getParcelInfo = async function (baseUrl, parcelViewerURL, parcelNum) {
+
+	let buildingTableIDs = ['#cphContent_DetailsViewPropTypeR', '#cphContent_DetailsViewPropTypeC', '#cphContent_DetailsViewPropTypeK'];
+
+	// List of land tables IDs on the property detail page
+	let landTableIds = ['#cphContent_DetailsViewLand', '#cphContent_DetailsViewLandSystem',  '#cphContent_DetailsViewLandViews', 
+               '#cphContent_DetailsViewLandWaterfront', '#cphContent_DetailsViewLandDesignations', 
+               '#cphContent_DetailsViewLandNuisances',
+               '#cphContent_DetailsViewLandProblems', '#cphContent_DetailsViewLandEnviro']
+
+	// create parcel url
+	let parcelURL = baseUrl.concat(parcelNum);
+
+		// instantiate the driver
+ 	let driver = new webdriver.Builder()
+ 	    .forBrowser(webdriver.Browser.CHROME)
+ 	    .build();
+
+	// instantiate empty object to hold this current parcel
+	let currentParcel = {
+		'Basic' : {},
+		'Building' : {},
+		'Tax' : {},
+		'Location' : {},
+		'Land' : {}
+
+	};
+
+	try {
+			await driver.get(parcelURL);
+			console.log(parcelURL);
+
+			// Add the Parcel number and property info link
+			currentParcel['Basic']['PARCEL_NUM'] = parcelNum;
+			currentParcel['Basic']['PROPERTY_INFO_LINK'] = parcelURL;
+
+			// get the rows from the first table of the parcel information page
+	        let parcelTableRows = await driver.findElement(By.css('#container'))
+	        							.findElement(By.css('#maincol'))
+	        							.findElement(By.css('#contentcol'))
+	        							.findElement(By.css('._table2'))
+	        							.findElement(By.css('.GridViewStyle'))
+	        							.findElement(By.tagName('tbody'))
+	        							.findElements(By.tagName('tr'))
+
+	        // loop through the first table of the parcel information page and add key value pairs to 'Basic' property.
+	        for (const row of parcelTableRows) {
+
+	        	let cells = await row.findElements(By.tagName('td'));
+
+	        	let key = await cells[0].getAttribute('innerText');
+	        	let value = await cells[1].getAttribute('innerText');
+
+	        	currentParcel['Basic'][key] = value;
+	        }
+
+
+	        // handles errors due to noSuchElement for the different types of property tables
+	        // GET BUILDING INFORMATION
+	        // loop through buildingTableIDs list until building table is found
+	        for (const buildingTableID of buildingTableIDs) {
+	        	console.log(buildingTableID);
+		    	try {
+		    		let buildingTableRows = await driver.findElement(By.css('#container'))
+		    										.findElement(By.css('#maincol'))
+		    										.findElement(By.css('#contentcol'))
+		    										.findElement(By.css('._table2'))
+		    										.findElement(By.css(buildingTableID))
+		    										.findElement(By.tagName('tbody'))
+		    										.findElements(By.tagName('tr'))
+
+		    		// loop through the buildingTableRows and add key value pairs to 'Building' property.
+		    		for (const row of buildingTableRows) {
+		    			let cells = await row.findElements(By.tagName('td'));
+
+		    			let key = await cells[0].getAttribute('innerText');
+		    			let value = await cells[1].getAttribute('innerText');
+
+		    			currentParcel['Building'][key] = value;
+		    		}
+
+		    		// if the above block was successful, buildingInfo has been found, now we won't check the other table names.
+		    		break;
+
+		    	} catch (error) {
+		    		console.log(error.name);
+		    	}
+	    	}
+
+	    	// Locate tax table
+	    	try {
+	    		let taxTableRows = await driver.findElement(By.css('#container'))
+	    										.findElement(By.css('#maincol'))
+	    										.findElement(By.css('#contentcol'))
+	    										.findElement(By.css('._table2'))
+	    										.findElement(By.css('#cphContent_GridViewDBTaxRoll'))
+	    										.findElement(By.tagName('tbody'))
+	    										.findElements(By.tagName('tr'))
+
+	    		// grab only the first row from the tax table (skipping headers at index 0)
+	    		let taxFirstRow = await taxTableRows[1].findElements(By.tagName('td'));
+
+	    		// parse text values into integers for tax information
+	    		let taxTotal = await taxFirstRow[taxFirstRow.length - 1].getAttribute('innerText');
+	    		let taxLand = await taxFirstRow[taxFirstRow.length - 3].getAttribute('innerText');
+	    		let taxBuilding = await taxFirstRow[taxFirstRow.length - 2].getAttribute('innerText');
+
+	    		// Add tax info to current parcel object
+	    		currentParcel['Tax']['TAXABLE_TOTAL'] = parseInt(taxTotal.replace(/[^0-9]/g, ""));
+	    		currentParcel['Tax']['TAXABLE_LAND'] = parseInt(taxLand.replace(/[^0-9]/g, ""));		
+	    		currentParcel['Tax']['TAXABLE_BUILDING'] = parseInt(taxBuilding.replace(/[^0-9]/g, ""));
+
+	    	} catch (error) {
+	    		console.log(error);
+	    	}
+
+    	// Get Land information
+    	// parse acreage to float and assign to key 'Acres'
+	    try {
+		    // Find and click the property detail button
+		    let propertyDetailLinkButton = await driver.findElement(By.css('#cphContent_LinkButtonDetail'));
+
+		     	// instantiate actions
+		 	const actions = driver.actions({async: true});
+
+			// Click the property detail button then wait for it to load
+		 	await actions.move({origin:propertyDetailLinkButton})
+		 					.click()
+		 					.pause(3000)
+		 					.perform();
+
+		 	await actions.clear();
+
+		 	// Start by getting the base table that all the land tables are in
+		 	let landTables = await driver.findElement(By.css('#container'))
+                        .findElement(By.css('#maincol'))
+                        .findElement(By.css('#TABLE1'))
+                        .findElement(By.tagName('tbody'));
+
+            // loop through the land table IDs
+            for (const tableID of landTableIds) {
+            	let currTable = await landTables.findElement(By.css(tableID)).findElement(By.tagName('tbody'));
+
+            	let currTableRows = await currTable.findElements(By.tagName('tr'));
+
+            	// loop through the current table's rows
+            	for (const row of currTableRows) {
+            		let cells = await row.findElements(By.tagName('td'));
+
+            		let key = await cells[0].getAttribute('innerText');
+
+            		let value = await cells[1].getAttribute('innerText');
+
+            		// parse Acres from a string to a float
+            		if (key == 'Acres') {
+            			value = parseFloat(value);
+            		}
+            		currentParcel['Land'][key] = value;
+            	}
+            }
+
+	 	} catch (error) {
+	 		console.log(error);
+	 	}
+
+
+    } catch(error) {
+    	console.log(error.name);
+    } finally {
+    	driver.quit();
+    }
+
+    return currentParcel;
+}
+
+module.exports = {getParcelInfo};
