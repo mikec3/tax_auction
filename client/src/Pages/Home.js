08d818094a3dab3c6beb6e1963afc00037810674
Michael Couch <michaelcouch@MacBook-Pro.local>
2023-04-06 21:55:50 -0700

    Added Routes in client. App.js turned into Router and index points to Pages/Home component

---
+++ client/src/Pages/Home.js
@@ -0,0 +1,82 @@
+import logo from '../logo.svg';
+import '../App.css';
+import React, {useState, useEffect} from 'react'
+import MyMap from '../MyMap.js'
+import HeaderBar from '../HeaderBar.js'
+import InfoCard from '../InfoCard.js'
+import Filters from '../Filters.js'
+import io from 'socket.io-client';
+import {ParcelListProvider} from '../ParcelListContext';
+import GetData from '../GetData'
+
+function Home() {
+
+const [showFilters, setShowFilters] = useState(false);
+const [filtersClass, setFilterClass] = useState('FiltersHide');
+
+const [newUserRegistered, setNewUserRegistered] = useState(false);
+const [socket, setSocket] = useState();
+const [googleMapsAPIKey, setGoogleMapsAPIKey] = useState();
+
+// establish socket port if we haven't done it already
+useEffect(() =>  {
+
+	// only establish socket port if we haven't done it already
+	if (!newUserRegistered){
+		// establish socket port
+		setSocket(io());
+		setNewUserRegistered(true);
+	}
+
+	// only if socket is already established do we setup the message reciever
+	if (socket) {
+		// Process socket messages from server.
+		// returns api key for google map
+		socket.on('message', (res) => {
+		  //console.log(res);
+		  handleSocketMessage(res);
+		})
+	}
+
+}, [socket])
+
+	const handleSocketMessage = (res) => {
+		if(res.googleMapsAPIKey) {
+			setGoogleMapsAPIKey(res.googleMapsAPIKey);
+			console.log('setting google maps api key');
+		}
+	}
+
+	// either the filter button was pressed, or the 'X' close filters button was pressed, toggle the state.
+	const filterButtonPressed = () => {
+		console.log(showFilters);
+		console.log(filtersClass);
+
+		if (showFilters) {
+			setShowFilters(false);
+			setFilterClass('FiltersHide');
+		} else {
+			setShowFilters(true);
+			setFilterClass('FiltersShow');
+		}
+	}
+
+
+  return (
+    <div className="App">
+    	<ParcelListProvider>
+    	<GetData/>
+		    <HeaderBar/>
+		    {googleMapsAPIKey && (
+		    		<div className="mapCard">
+			    		<MyMap googleMapsAPIKey={googleMapsAPIKey} filtersClass={filtersClass}/>
+					    <InfoCard filterButtonPressed={filterButtonPressed}/>
+					    <Filters className={filtersClass} filterButtonPressed={filterButtonPressed}/>
+			    	</div>
+			    	)}
+	    </ParcelListProvider>
+    </div>
+  )
+}
+
+export default Home;
