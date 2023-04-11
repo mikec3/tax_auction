f79b9e61b72d896cee17adf6137c66ae085d3ba3
Michael Couch <michaelcouch@MacBook-Pro.local>
2023-04-10 20:12:13 -0700

    ParcelPage now reads url parameters and filters parcel list from parcel list context for the parcel num in the url

--- client/src/Pages/ParcelPage.js
+++ client/src/Pages/ParcelPage.js
@@ -1,17 +1,31 @@
 import React, {useState, useEffect} from 'react'
-import MyMap from '../MyMap.js'
 import HeaderBar from '../HeaderBar.js'
-import InfoCard from '../InfoCard.js'
-import Filters from '../Filters.js'
-import io from 'socket.io-client';
-import GetData from '../GetData'
+import {useList, useListDispatch} from '../ParcelListContext';
+import GetData from '../GetData' // need to include this to initialize the parcel list context
+
 
 function ParcelPage() {
+
+	//get parcel list from context
+	let parcelList = useList();
+
+	console.log(parcelList);
+
+	let searchParam = new URLSearchParams(window.location.search);
+
+	let searchParamParcelNum = searchParam.get('parcel');
+
+	console.log(searchParamParcelNum);
+	//console.log(searchParam.toString());
+
+	if (typeof parcelList != 'undefined') {
+		console.log(parcelList.filter(item=> item.Basic.PARCEL_NUM == searchParamParcelNum));
+	}
 
 
   return (
     	<React.Fragment>
-	    	<GetData/>
+    			<GetData/>
 			    <HeaderBar/>
 			    <p> hellooo!! </p>
 	    </React.Fragment>
