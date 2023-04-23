896e6208d02deeb62cc58d486f10ea803ab2b723
Michael Couch <michaelcouch@MacBook-Pro.local>
2023-04-22 18:49:07 -0700

    parcel share button now says `Copied` after clicking, to indicate link was sent to clipboard

--- client/src/ParcelShareButton.js
+++ client/src/ParcelShareButton.js
@@ -3,17 +3,20 @@ import React, {useState, useEffec, useRef} from 'react'
 
 
 function ParcelShareButton(props) {
+
+	const [buttonText, setButtonText] = useState('Share');
 	
 const shareParcel = () => {
 	console.log(props.parcel);
 	navigator.clipboard.writeText("https://www.countyauctionproperties.com/ParcelPage?parcel="+props.parcel.Basic.PARCEL_NUM)
+	setButtonText('Copied');
 }
 
 return (
 	<div>
 		<button className='button-17'
 			onClick={shareParcel}>
-			Share 
+			{buttonText} 
 		</button>
 	</div>
 	);
