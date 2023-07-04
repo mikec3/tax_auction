a76ef4334b04cbb4c0d0945f00e37dfab241f40f
Michael Couch <michaelcouch@MacBook-Pro.local>
2023-07-04 15:13:22 -0700

    headerBar title now links to homepage

--- client/src/HeaderBar.js
+++ client/src/HeaderBar.js
@@ -5,7 +5,9 @@ const HeaderBar = () => {
 
 return (
 	<div className='HeaderBar'>
-	<h3> Washington State County Tax Foreclosure Properties </h3>
+		<a style={{ textDecoration: 'none' , color: 'black'}} href='https://www.countyauctionproperties.com'>
+			<h3> Washington State County Tax Foreclosure Properties </h3>
+		</a>
 	</div>
 )
 
